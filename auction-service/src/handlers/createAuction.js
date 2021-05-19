473f45a45b3abbe0e1f6d8db7b1b0bdf3417d882
RdotSilva <RyandotSilva@gmail.com>
2021-05-19 18:59:52 -0400

    Add logic to populate new auction

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,7 +1,19 @@
 const createAuction = async (event, context) => {
+  // Get the body data from the event
+  const { title } = JSON.parse(event.body);
+
+  // Get current date
+  const now = new Date();
+
+  // Create new auction item
+  const auction = {
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 };
 
