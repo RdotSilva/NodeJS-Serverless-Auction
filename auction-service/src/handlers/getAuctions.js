d3d814a87ed4062047bc6a8559328c1b3adba347
RdotSilva <RyandotSilva@gmail.com>
2021-05-22 16:50:48 -0400

    Add getAuctions method

---
+++ auction-service/src/handlers/getAuctions.js
@@ -0,0 +1,32 @@
+import AWS from "aws-sdk";
+
+import createError from "http-errors";
+
+// Create new DynamoDB instance
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+const getAuctions = async (event, context) => {
+  let auctions;
+
+  try {
+    // Query the database for all auctions
+    const result = await dynamoDB
+      .scan({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+      })
+      .promise();
+
+    // Get the items of the database query
+    auctions = result.Items;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auctions),
+  };
+};
+
+export const handler = getAuctions;
