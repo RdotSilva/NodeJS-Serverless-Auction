110eb2ae5aaeefa7e390cef8dca6df213d93314e
RdotSilva <RyandotSilva@gmail.com>
2021-05-21 20:49:07 -0400

    Add middleware wrapper

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,5 +1,9 @@
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
 
 // Create new DynamoDB instance
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
@@ -33,4 +37,7 @@ const createAuction = async (event, context) => {
   };
 };
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
