5639ee1ebe0deb5a2a9a7755bdcdd52793c7e3bc
RdotSilva <RyandotSilva@gmail.com>
2021-05-23 19:20:30 -0400

    Add commonMiddleware wrapper

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,8 +1,5 @@
 import AWS from "aws-sdk";
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
+import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
 
 // Create new DynamoDB instance
@@ -32,7 +29,4 @@ const getAuctions = async (event, context) => {
   };
 };
 
-export const handler = middy(getAuctions)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(getAuctions);
