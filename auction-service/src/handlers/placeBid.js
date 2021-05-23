0bbe9416a122ccdcf066d6522091a72b4b42d39d
RdotSilva <RyandotSilva@gmail.com>
2021-05-23 19:30:13 -0400

    Add logic to handle updating auction w/ new bid

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -6,6 +6,31 @@ import createError from "http-errors";
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 const placeBid = async (event, context) => {
+  // Get auction ID from path params
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: "set highestBid.amount = :amount",
+    ExpressionAttributeValues: {
+      ":amount": amount,
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
+  let updatedAuction;
+
+  try {
+    const result = await dynamoDB.update(params).promise();
+
+    // Get all of the attributes of the object that was updated
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify(updatedAuction),
