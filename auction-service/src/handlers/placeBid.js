39e7a1c971c29ecd5e7d6d92fdbe156bc0d65a6d
RdotSilva <RyandotSilva@gmail.com>
2021-05-24 19:26:56 -0400

    Add validation logic to check highest bid amount

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
+import { getAuctionById } from "./getAuction";
 
 // Create new DynamoDB instance
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
@@ -9,6 +10,16 @@ const placeBid = async (event, context) => {
   // Get auction ID from path params
   const { id } = event.pathParameters;
   const { amount } = event.body;
+
+  const auction = await getAuctionById(id);
+
+  // Validation for bid amount
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden(
+      `Your bid must be higher than ${auction.highestBid.amount}`
+    );
+  }
+
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
