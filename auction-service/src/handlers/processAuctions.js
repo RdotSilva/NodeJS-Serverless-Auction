2ec8089534eed2bc2633e5ae37c8bcf20b2f09df
RdotSilva <RyandotSilva@gmail.com>
2021-05-26 19:28:46 -0400

    Add logic to close ended auctions

--- auction-service/src/handlers/processAuctions.js
+++ auction-service/src/handlers/processAuctions.js
@@ -1,9 +1,24 @@
+import createError from "http-errors";
+import { closeAuction } from "../lib/closeAuction";
 import { getEndedAuctions } from "../lib/getEndedAuctions";
 
 const processAuctions = async (event, context) => {
-  const auctionsToClose = await getEndedAuctions();
+  try {
+    // Find all auctions that have ended
+    const auctionsToClose = await getEndedAuctions();
 
-  console.log(auctionsToClose);
+    // Create promise array for all auctions that need to be closed
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction)
+    );
+
+    await Promise.all(closePromises);
+
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 };
 
 export const handler = processAuctions;
