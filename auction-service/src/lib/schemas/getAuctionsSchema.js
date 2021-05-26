08e756a3dbfc2bc7a34c4e849d027d8e7a6ce29a
RdotSilva <RyandotSilva@gmail.com>
2021-05-26 19:47:13 -0400

    Add schema config

---
+++ auction-service/src/lib/schemas/getAuctionsSchema.js
@@ -0,0 +1,19 @@
+// JSON Auction schema validation
+const schema = {
+  type: "strict",
+  properties: {
+    queryStringParameters: {
+      type: "object",
+      properties: {
+        status: {
+          type: "string",
+          enum: ["OPEN", "CLOSED"],
+          default: "OPEN",
+        },
+      },
+    },
+  },
+  required: ["queryStringParameters"],
+};
+
+export default schema;
