7588d3ecd44ea13e48605fbbd338b215ab6f1cfd
RdotSilva <RyandotSilva@gmail.com>
2021-05-30 21:35:41 -0400

    Add test email params

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,25 @@
+import AWS from "aws-sdk";
+import { notificationEmailSource } from "../../config";
+
+const ses = new AWS.SES({ region: "us-east-1" });
+
 const sendMail = async (event, context) => {
-  console.log(event);
-  return event;
+  const params = {
+    Source: notificationEmailSource,
+    Destination: {
+      ToAddresses: [notificationDestinationEmail],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello World",
+        },
+      },
+      Subject: {
+        Data: "Test Mail",
+      },
+    },
+  };
 };
 
 export const handler = sendMail;
