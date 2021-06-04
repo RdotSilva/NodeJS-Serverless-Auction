6a30a1afc568488271a38802300de989602c2e50
RdotSilva <RyandotSilva@gmail.com>
2021-06-04 18:14:30 -0400

    Add logic to create base64 image

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,16 @@
+import { getAuctionById } from "./getAuction";
+
 const uploadAuctionPicture = async (event) => {
+  // Get the auction ID
+  const { id } = event.pathParameters;
+
+  // Find the auction
+  const auction = await getAuctionById(id);
+
+  // Create base64 image
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
