04b152e73b4ae43a2fb63306b76197ee05c5f4e8
RdotSilva <RyandotSilva@gmail.com>
2021-06-05 22:06:26 -0400

    Add try/catch error handling

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,7 +1,8 @@
-import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
-import { getAuctionById } from "./getAuction";
 import middy from "@middy/core";
 import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import { getAuctionById } from "./getAuction";
 
 const uploadAuctionPicture = async (event) => {
   // Get the auction ID
@@ -14,8 +15,16 @@ const uploadAuctionPicture = async (event) => {
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
 
-  const uploadToS3Result = await uploadPictureToS3(auction.id + ".jpg", buffer);
-  console.log(uploadToS3Result);
+  try {
+    const uploadToS3Result = await uploadPictureToS3(
+      auction.id + ".jpg",
+      buffer
+    );
+    console.log(uploadToS3Result);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 200,
