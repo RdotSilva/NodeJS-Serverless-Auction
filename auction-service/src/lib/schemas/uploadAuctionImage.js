2aa3a93240119735fcb77182d4a14a6b405b965e
RdotSilva <RyandotSilva@gmail.com>
2021-06-06 20:42:10 -0400

    Add schema

---
+++ auction-service/src/lib/schemas/uploadAuctionImage.js
@@ -0,0 +1,13 @@
+// JSON Create Auction schema validation
+const schema = {
+  type: "object",
+  properties: {
+    body: {
+      type: "string",
+      minLength: 1,
+    },
+  },
+  required: ["body"],
+};
+
+export default schema;
